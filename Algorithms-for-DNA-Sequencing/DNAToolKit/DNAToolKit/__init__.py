762cf1520cedd6b0e0d332e88632a6baa83acf65
raulminan <raulminan@gmail.com>
2022-10-17 19:06:45 +0200

    chore: add imports for new modules

--- Algorithms-for-DNA-Sequencing/DNAToolKit/DNAToolKit/__init__.py
+++ Algorithms-for-DNA-Sequencing/DNAToolKit/DNAToolKit/__init__.py
@@ -1,4 +1,4 @@
-from .read_files import functions
-from .index import functions 
-
-## test if the above are necessary
+from .read_files import read_functions
+from .index import index_functions 
+from .naive import naive_functions
+from .boyer_moore import boyer_moore_functions
