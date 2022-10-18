3119b6cd6bbe2d5b50dbbad79051d53dae55ed95
raulminan <raulminan@gmail.com>
2022-10-18 19:33:54 +0200

    feat: add tests for naive module

---
+++ Algorithms-for-DNA-Sequencing/DNAToolKit/tests/test_naive/test_naive.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+
+from DNAToolKit import naive
+
+class Tests:
+    "tests for the naive module"
+    def test_naive_matching(self):
+        pattern = "CTGT"
+        ten_as = "AAAAAAAAAA"
+        text = ten_as + "CTGT" + ten_as + "CTTT" + ten_as + "CGGG" + ten_as
+        mismatches = 2
+
+        occurrences = naive.naive_matching(pattern, text, mismatches)
+
+        assert occurrences == [10, 24, 38]
